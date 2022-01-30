20c3dc78912aaeea0ae38e6652720a0387a93e11
Luca Haneklau <git@luca.lsys.ac>
2022-01-30 10:49:01 +0100

    fixed reading long ssh keys from device

--- plugins/terminal/vyos.py
+++ plugins/terminal/vyos.py
@@ -67,7 +67,7 @@ class TerminalModule(TerminalBase):
 
     def on_open_shell(self):
         try:
-            for cmd in (b"set terminal length 0", b"set terminal width 512"):
+            for cmd in (b"set terminal length 0", b"set terminal width 1024"):
                 self._exec_cli_command(cmd)
             self._exec_cli_command(
                 b"set terminal length %d" % self.terminal_length
